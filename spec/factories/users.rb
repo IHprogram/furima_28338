8b166c69b7933980eca82d0dbad0b8b743fd4d1a
IHprogram <ihm20202020@gmail.com>
2020-08-17 21:35:21 +0900

    FactoryBotでユーザーの値を指定

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Internet.username}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    family_name {Gimei.last.kanji}
+    first_name {Gimei.first.kanji}
+    family_name_furigana { Gimei.last.katakana }
+    first_name_furigana { Gimei.first.katakana }
+    birth_day { Faker::Date.birthday }
+  end
+end
