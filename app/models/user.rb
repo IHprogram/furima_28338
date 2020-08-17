9031f4392dcab16894961cbec9585028ef5652e1
IHprogram <ihm20202020@gmail.com>
2020-08-17 11:51:37 +0900

    空のデータを保存できないようにするバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :items
   has_many :buyers
+
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_furigana, presence: true
+  validates :family_name_furigana, presence: true
+
+  
+
+
+
 end
