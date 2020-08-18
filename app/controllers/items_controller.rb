a8ba992701e9881d8f6c737ae228f120d7091568
IHprogram <ihm20202020@gmail.com>
2020-08-18 22:02:18 +0900

    itemsコントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
   end
-
+  
   def new
+  end
+  
+  def create
+    # Post.create(カラム名: params[:値])
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
