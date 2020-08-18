e2a406dd9889230a638b4bb09f7f492ef568b086
IHprogram <ihm20202020@gmail.com>
2020-08-18 11:54:06 +0900

    Rubocopによる自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,17 +10,16 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :birth_day, presence: true
 
-  with_options presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :first_name
     validates :family_name
   end
 
-  with_options presence: true, format:{ with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
     validates :first_name_furigana
     validates :family_name_furigana
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
-
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
