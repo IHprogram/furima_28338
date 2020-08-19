d037030893836b79f9542af71bc47e5a9c3c6f1c
IHprogram <ihm20202020@gmail.com>
2020-08-19 18:11:51 +0900

    index、new、createアクションを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
   def index
   end
   
@@ -7,17 +6,6 @@ class ItemsController < ApplicationController
   end
   
   def create
-    # Post.create(カラム名: params[:値])
   end
 
-  def destroy
-  end
-  
-  private
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
