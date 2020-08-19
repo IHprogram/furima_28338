652753a9b72059a626eceb82c67e47ad2ddcf03b
IHprogram <ihm20202020@gmail.com>
2020-08-19 21:09:41 +0900

    バリデーションの追加と整理
    ・全カラムをwith_options presence: true do ~ end内にまとめた。
    
    ・priceカラムの範囲を、「300~9999999円」に指定。
    
    ・priceカラムに、「半角数字のみでの入力」という正規表現を指定。
    
    ・category_id、status_id、fee_id、prefecture_id、shippingdate_idにエラーメッセージを指定。

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :fee
   belongs_to_active_hash :prefecture
 
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id,     numericality: { other_than: 1 } 
-  validates :status_id,       numericality: { other_than: 1 } 
-  validates :shippingdate_id, numericality: { other_than: 1 } 
-  validates :fee_id,          numericality: { other_than: 1 } 
-  validates :prefecture_id,   numericality: { other_than: 1 } 
 
-    #空欄では保存できないようにする
-    validates :name, :content, :price, :category, :status, :shippingdate, :fee, :prefecture, :image, presence: true
-
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :content
+    validates :price,           format: {with: /\A[0-9]\z/, message: 'Half-width number'}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: 'Out of setting range'}
+    validates :category_id,     numericality: { other_than: 1, message: 'Select' } 
+    validates :status_id,       numericality: { other_than: 1, message: 'Select' } 
+    validates :fee_id,          numericality: { other_than: 1, message: 'Select' } 
+    validates :prefecture_id,   numericality: { other_than: 1, message: 'Select' } 
+    validates :shippingdate_id, numericality: { other_than: 1, message: 'Select' } 
+  end
 end
