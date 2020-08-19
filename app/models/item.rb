387de2612e5b0c6dde2d2cd9f636843c5e0fa252
IHprogram <ihm20202020@gmail.com>
2020-08-19 17:15:37 +0900

    ActiveHashを利用するカラムに対するバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buyer
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shippingdate
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :prefecture
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id,     numericality: { other_than: 1 } 
+  validates :status_id,       numericality: { other_than: 1 } 
+  validates :shippingdate_id, numericality: { other_than: 1 } 
+  validates :fee_id,          numericality: { other_than: 1 } 
+  validates :prefecture_id,   numericality: { other_than: 1 } 
+
 end
