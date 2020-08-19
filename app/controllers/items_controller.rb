4139c558939193f8803973d7d4a4154f3b52f00b
IHprogram <ihm20202020@gmail.com>
2020-08-19 18:17:40 +0900

    出品した商品の投稿内容を保存する処理の記述
    createアクション内には、
    「保存される場合はトップページへ戻り、保存されない場合は出品する商品の投稿ページへ戻る」
    という条件分岐式を記述した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,22 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :category_id, :status_id, :shippingdate_id, :fee_id, :prefecture_id, :image).merge(user_id: current_user.id)
   end
 
 end
