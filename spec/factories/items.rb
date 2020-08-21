0d5c69334799754342dccc8a312152c66419ae0b
IHprogram <ihm20202020@gmail.com>
2020-08-21 21:22:33 +0900

    ダミーテキストとアソシエーションの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
+    # imageカラムはここでは設定しない
+    name { Faker::Lorem.words }
+    content { Faker::Lorem.sentence }
+    price { Faker::Number.between(from: 300, to: 9999999) }
     
+    association :user
+    category { Category.all.sample }
+    status { Status.all.sample }
+    shippingdate { Shippingdate.all.sample }
+    fee { Fee.all.sample }
+    prefecture { Prefecture.all.sample }
+
   end
 end
