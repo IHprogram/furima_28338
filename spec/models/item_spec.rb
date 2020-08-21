25d03a6aff34d4080630b41f1f10a44ae06d658c
IHprogram <ihm20202020@gmail.com>
2020-08-21 21:23:28 +0900

    Itemモデルの単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('public/images/sample.png')
+    end
+    
+    it 'name,content,category_id,status_id,shippingdate_id,fee_id,prefecture_id,priceが存在すれば登録できること' do
+      expect(@item).to be_valid
+    end
+
+    it 'imageが空では登録できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'nameが空では登録できないこと' do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'contentが空では登録できないこと' do
+      @item.content = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Content can't be blank")
+    end
+
+    it 'priceが空では登録できないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'categoryは「---」を選択すると登録できないこと' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
+    it 'statusは「---」を選択すると登録できないこと' do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status Select")
+    end
+
+    it 'feeは「---」を選択すると登録できないこと' do
+      @item.fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Fee Select")
+    end
+
+    it 'prefectureは「---」を選択すると登録できないこと' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture Select")
+    end
+
+    it 'shippingdateは「---」を選択すると登録できないこと' do
+      @item.shippingdate_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shippingdate Select")
+    end
+    
+    it 'priceが300円未満なら登録できないこと' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it 'priceが9999999円より高いなら登録できないこと' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+  end
 end
