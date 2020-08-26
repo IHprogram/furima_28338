4235de830f07f4f8d27736ebaeac1c9ae805f460
IHprogram <ihm20202020@gmail.com>
2020-08-26 10:43:12 +0900

    building_nameカラムの修正

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_123205) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "house_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "buyer_id", null: false
     t.datetime "created_at", precision: 6, null: false
