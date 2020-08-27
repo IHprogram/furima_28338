331cb0aea7f6bbeb44f1ce50c08adf650e62638b
IHprogram <ihm20202020@gmail.com>
2020-08-27 09:25:45 +0900

    クレジット決済機能を利用するための記述

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,12 +1,34 @@
 class BuyersController < ApplicationController
 
   def index
-    @buyer = Item.find(params[:item_id])
+    @item = Item.find(params[:item_id])
+    @buyer = Buyer.new
   end
 
   def create
+    @buyer = Buyer.new
+    if pay_item
+      @buyer.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
+
+  def buyer_params
+    params.permit(:token).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: buyer_params[:token],
+      currency:'jpy'
+    )
+  end
 
 end
