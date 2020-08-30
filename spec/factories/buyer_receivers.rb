1b383699e2156da3863883b425bd256e6c56bc0b
IHprogram <ihm20202020@gmail.com>
2020-08-30 12:34:12 +0900

    ダミーテキストの修正と追加

--- spec/factories/buyer_receivers.rb
+++ spec/factories/buyer_receivers.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :buyer_receiver do
+    token {"tok_xxxxxxxxxxxxxxxxxxxxxxxxxxx"}
     postal_code { "123-4567" }
-    prefecture { Prefecture.all.sample }
+    prefecture_id { 2 }
     city { Gimei.city.kanji }
     house_number { Gimei.town.kanji }
     building_name { "東京ハイツ" }
     phone_number { Faker::Number.number(digits: 11) }
-    association :buyer
   end
 end
